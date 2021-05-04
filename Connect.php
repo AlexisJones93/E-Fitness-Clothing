eb3e3f71315f0596450cc763661905d66243880d
Alexis Jones <21406612@bucks.ac.uk>
2021-05-04 19:14:18 +0100

    Database connection updated to test database.

--- Connect.php
+++ Connect.php
@@ -1,6 +1,6 @@
 <?php  
 
-$conn=mysqli_connect("localhost", "root","","e_fitness") or die("The connection to the database cannot be established");
+$conn=mysqli_connect("localhost", "root","","test") or die("The connection to the database cannot be established");
 
 
 ?>
